1d8f61a911d76a3abead92286d13c73478385f27
dongli <512354087@qq.com>
2020-09-10 16:12:20 +0800

    feat(init): init project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // 箭头函数只有一个参数的时候可以忽略括号
+    // arrowParens: 'avoid',
+    // 括号内部不要出现空格
+    // bracketSpacing: false,
+    printWidth: 120, //行宽
+    semi: false, // 是否需要分号
+    singleQuote: true, // 使用单引号
+    tabWidth: 2, // 摩纳哥缩进 2
+    trailingComma: 'none' //后置逗号 多行对象、数组在最后一行增加逗号
+}
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
